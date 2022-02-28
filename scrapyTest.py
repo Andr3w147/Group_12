95ac44d1a8313c0ac3c96fd2387dda365e159c83
Andr3w147 <100426320+Andr3w147@users.noreply.github.com>
2022-02-28 23:33:52 +0800

    Update scrapyTest.py

--- scrapyTest.py
+++ scrapyTest.py
@@ -15,11 +15,3 @@ class NewSpider(scrapy.Spider):
                 'Image Link': x.xpath(newsel).extract_first(),
             }
 
-# To recurse next page
-        Page_selector = '.next a ::attr(href)'
-        next_page = response.css(Page_selector).extract_first()
-        if next_page:
-            yield scrapy.Request(
-                response.urljoin(next_page),
-                callback=self.parse
-        )
